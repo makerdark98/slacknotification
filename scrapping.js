0c978e8aaefa73b4bff53ae62b4c75afc1a664be
md98 <makerdark98@naver.com>
2017-07-26 04:20:45 +0900

    Add Comment, Sqlite3 module

--- scrapping.js
+++ scrapping.js
@@ -1,7 +1,7 @@
 /**
  * Created by md98 on 17. 7. 26.
  */
-
+// Logger Configure
 var winston = require('winston');
 
 var logger = new (winston.Logger)({
@@ -10,56 +10,54 @@ var logger = new (winston.Logger)({
         new (winston.transports.File)({ filename: 'scrapping.log'})
     ]
 });
-logger.log('info', 'Scrapping js logging start');
+//
 
+
+logger.log('Info', 'Scrapping js logging start');
+
+// Read ICT Page
 var request = require('request');
 var cheerio = require("cheerio");
 var ict_url = "http://ict.cau.ac.kr/20150610/sub05/sub05_01_list.php"
 
-
 request(ict_url, function(error, response, body){
-    try {
-        if (error) {
-            logger.log('error', "request error");
-            throw error;
-        }
-
-        var $ = cheerio.load(body, {
-            normalizeWhitespace: true
-        });
-        var postElements = $("table.board_list_type01 tbody tr");
-        postElements.each(function () {
-            var children = $(this).children();
-            var row = {
-                "number": Number($(children[0]).find('a').attr('href').replace(/[^0-9]/g, '')),
-                "title": $(children[1]).text().replace(/\t|\n/g, ''),
-                "date": $(children[2]).text()
-            };
-            console.log(row);
-        });
-    }
-    catch(exception){
-        logger.log('error', error);
-    }
-});
-
-var mysql = require('mysql');
-var fs = require('fs');
-var connection = mysql.createConnection({
-    host    : 'localhost',
-    user    : 'md98',
-    password: fs.readFile('dbpassword.txt','utf-8', function(error, data){
-        if(error) throw error;
-        return data;
-    }),
-    database: 'data.db'
-});
-/*
-connection.connect(function(error){
-    if(error){
+    if (error) {
+        logger.log('Error', "request error");
         throw error;
     }
-    else
+
+    var $ = cheerio.load(body, {
+        normalizeWhitespace: true
+    });
+    var postElements = $("table.board_list_type01 tbody tr");
+    postElements.each(function () {
+        var children = $(this).children();
+        var row = {
+            "number": Number($(children[0]).find('a').attr('href').replace(/[^0-9]/g, '')),
+            "title": $(children[1]).text().replace(/\t|\n/g, ''),
+            "date": $(children[2]).text()
+        };
+    });
+
 });
-*/
-logger.log('info', 'Scrapping js logging End');
+
+
+// Read Database Password File
+var fs = require('fs');
+var dbpassword;
+logger.log('Info', 'Start to read dbpassword.txt');
+fs.readFile('dbpassword.txt', 'utf-8', function (error, data) {
+    dbpassword = data;
+    logger.log('Info', 'End to read dbpassword.txt');
+});
+
+// Connect Database
+
+//ToDo : db Connection, db design
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('data.db');
+db.serialize(function(){
+
+});
+
+logger.log('Info', 'Scrapping js logging End');
