77c850d682ecba69b6db16f48bdb2ae082a1def4
md98 <makerdark98@naver.com>
2017-09-14 18:47:24 +0900

    Fix init.sh

--- init.sh
+++ init.sh
@@ -11,8 +11,8 @@ npm update > init.log
 npm install selenium-webdriver > init.log
 
 
-MACHINE_TYPE=`uname -m`
-if [${MACHINE_TYPE} == 'x_86_64']; then
+MACHINE_TYPE=$(uname -m)
+if [ ${MACHINE_TYPE} == 'x_86_64' ]; then
     echo 'MACHINE_TYPE is x_86_64'
     echo 'Download chromedriver'
     wget https://chromedriver.storage.googleapis.com/2.31/chromedriver_linux64.zip
@@ -24,12 +24,12 @@ if [${MACHINE_TYPE} == 'x_86_64']; then
     sudo apt-get install -y google-chrome-stable
     sudo apt-get install -y default-jdk
 else
-    if [${MACHINE_TYPE} == 'armv7l']; then
+    if [ ${MACHINE_TYPE} == 'armv7l' ]; then
         echo 'MACHINE_TYPE is armv7l'
         echo 'chrome webdriver download'
         wget https://github.com/electron/electron/releases/download/v1.6.0/chromedriver-v2.21-linux-armv7l.zip
-        unzip chromedriver_linux-v2.21-linux-arm7l.zip
-        rm chromedriver_linux-v2.21-linux-arm7l.zip
+        unzip chromedriver-v2.21-linux-arm7l.zip
+        rm chromedriver-v2.21-linux-arm7l.zip
     else
         echo 'Error'
     fi
